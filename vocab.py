8e3e802ea54977f7e657284666ba861c3edfe0a9
ronrest <ronny.coding@gmail.com>
2017-07-20 19:04:45 +1000

    FEAT: Add vocab_tally_from_files()

--- vocab.py
+++ vocab.py
@@ -14,3 +14,26 @@ import glob
 from support import tokenization, Timer
 from file_support import file2str, list2file, file2list
 
+def vocab_tally_from_files(data_dir):
+    ext = "txt"
+    datasets = ["train", "test"]
+    tally = collections.Counter([])
+    for dataset in datasets:
+        timer = Timer()
+        for sentiment in ["neg", "pos"]:
+            timer.start()
+            print("Processing {} {} data".format(dataset, sentiment), end="")
+            
+            dir = os.path.join(data_dir, dataset, sentiment)
+            files = glob.glob(os.path.join(dir, "*.{}".format(ext)))
+            
+            # ITERATE THROUGH EACH FILE IN THE SUBDIR - and update the tally
+            for file in files:
+                text = file2str(file)
+                text = tokenization(text)
+                tally.update(text)
+                
+            print("-- DONE in {}".format(timer.elapsed_string()))
+    return tally
+
+
