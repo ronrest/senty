17b376186b5b1c2fd207058b9614a6ba84faac66
ronrest <ronny.coding@gmail.com>
2017-07-22 16:23:34 +1000

    FEAT: Add batch_predictions()

--- eval.py
+++ eval.py
@@ -1,3 +1,24 @@
 from support import batch_from_indices
 import numpy as np
 
+def batch_predictions(model, x, seq_maxlen=100, padval=0):
+    n_batch = len(x)
+    
+    # Set to evaluation mode, and store original training state
+    train_state = model.training
+    model.eval()
+    
+    # Initialize model
+    hidden = model.init_hidden(batch_size=n_batch)
+    model.zero_grad()
+    
+    # prepare data
+    X = batch_from_indices(x, ids=range(n_batch), maxlen=maxlen, padval=padval)
+    out, _ = model(X, hidden)
+    _, index = out.data.topk(1)
+    
+    # return model back to original training state
+    model.train(train_state)
+    
+    return index.view(-1).numpy()
+
