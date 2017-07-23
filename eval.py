0142a90044bda01c9a3cd804302027221d1b583e
ronrest <ronny.coding@gmail.com>
2017-07-23 19:36:06 +1000

    DOCS: Add docstring for batch_predictions()

--- eval.py
+++ eval.py
@@ -1,7 +1,25 @@
 from support import batch_from_indices
 import numpy as np
 
+# ==============================================================================
+#                                                              BATCH_PREDICTIONS
+# ==============================================================================
 def batch_predictions(model, x, seq_maxlen=100, padval=0):
+    """ Given a model and batch of data, it returns the predictions
+    
+    Args:
+        model:       (pytorch model)
+        x:           (list of list of ints)
+                     The input data, consisting of multiple sequences,
+                     where each sequence is a list of token ids.
+        seq_maxlen:  (int)(default=100)
+                     Trim/pad sequences to this length.
+        padval:      (int) (default=0)
+                     Value to use for padding.
+
+    Returns: (numpy array)
+        The array of predicted class ids for each sample in the batch.
+    """
     n_batch = len(x)
     
     # Set to evaluation mode, and store original training state
@@ -21,4 +39,5 @@ def batch_predictions(model, x, seq_maxlen=100, padval=0):
     model.train(train_state)
     
     return index.view(-1).numpy()
+
 
