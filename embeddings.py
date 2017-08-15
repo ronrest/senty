05c776d2bef21c8d984799585b10cd6d8ed3f1d5
ronrest <ronny.coding@gmail.com>
2017-08-15 21:42:39 +1000

    FEAT: Add ReviewsGenerator class

--- embeddings.py
+++ embeddings.py
@@ -1,4 +1,18 @@
 import numpy as np
+class ReviewsGenerator(object):
+    def __init__(self, rootdir):
+        self.rootdir = rootdir
+
+    def __iter__(self):
+        for subdir in ["pos", "neg", "unsup"]:
+            dirpath = os.path.join(self.rootdir, "train", subdir)
+            for filename in os.listdir(dirpath):
+                filepath = os.path.join(dirpath, filename)
+                with open(filepath , mode="r", encoding="utf-8") as fileobj:
+                    for line in fileobj:
+                        yield tokenization(line)
+
+
 def initialize_embeddings(n_words, embed_size):
     # INITIALIZE EMBEDDINGS TO RANDOM VALUES
     # TODO: Maybe play around with differeent initialization strategies
