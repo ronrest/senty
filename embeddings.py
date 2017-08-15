c66eaf260db3eae95832fa263ddb42cb109efd07
ronrest <ronny.coding@gmail.com>
2017-08-15 21:56:04 +1000

    FEAT: Add extract_glove_embeddings()

--- embeddings.py
+++ embeddings.py
@@ -40,4 +40,21 @@ def initialize_embeddings(n_words, embed_size):
     return weights
 
 
+def extract_glove_embeddings(file, n_words, embed_size, word2id):
+    weights = initialize_embeddings(n_words, embed_size)
+    
+    # EXTRACT DESIRED GLOVE WORD VECTORS FROM TEXT FILE
+    with open(file, encoding="utf-8", mode="r") as textFile:
+        for line in textFile:
+            # Extract the word, and embeddings vector
+            line = line.split()
+            word, vector = (line[0], np.array(line[1:], dtype=np.float32))
+            
+            # If it is in our vocab, then update the corresponding weights
+            id = word2id.get(word, None)
+            if id is not None:
+                weights[id] = vector
+    return weights
+
+
 
